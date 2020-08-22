6b6edda8a8d4aeef86f9683bf4baf5621a3fa6f2
aldom7673 <aldom7673@gmail.com>
2020-08-22 14:07:05 -0500

    Default Arguments

--- DefaultArguments/java/Main.java
+++ DefaultArguments/java/Main.java
@@ -1,8 +1,19 @@
 package DefaultArguments.java;
-import DefaultArguments.intro.FunctionsKt;
+//import DefaultArguments.intro.FunctionsKt;
 
 public class Main {
+
+    public static void modify_x (int x) {
+        x = 99;
+        System.out.print(x + " ");
+}
     public static void main(String [] args){
-        FunctionsKt.displaySeparator();
-    }
+        //FunctionsKt.displaySeparator();
+        int x = 22;
+        System.out.print(x + " ");
+        modify_x(x);
+        System.out.print(x + " ");
+}
+
+
 }
